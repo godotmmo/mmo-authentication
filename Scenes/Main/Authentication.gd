4e11ba0383aa613e1e1c490e08877c79cd02920b
Storm Mattern <storm.mattern@gmail.com>
2022-12-24 18:36:31 -0500

    Implement salting for hashing of players passwords

--- Scenes/Main/Authentication.gd
+++ Scenes/Main/Authentication.gd
@@ -29,20 +29,25 @@ func _Peer_Disconnected(gateway_id):
 @rpc(any_peer)
 func AuthenticatePlayer(username, password, player_id):
 	var token
+	var hashed_password
 	var gateway_id = multiplayer.get_remote_sender_id()
 	var result
 	print("Starting authentication")
-	if not PlayerData.player_ids[username].password == password:
-		print("Incorrect password")
+	if not PlayerData.player_ids.has(username):
 		result = false
 	else:
-		print("Successful authentication")
-		result = true
+		var retrieved_salt = PlayerData.player_ids[username].salt
+		hashed_password = GenerateHashedPassword(password, retrieved_salt)	
+		if not PlayerData.player_ids[username].password == hashed_password:
+			result = false
+		else:
+			result = true
+		
+			randomize()
+			token = str(randi()).sha256_text() + str(int(Time.get_unix_time_from_system()))
+			var gameserver = "GameServer1" # this will have to be replaced with a load balancer
+			GameServers.DistributeLoginToken(token, gameserver)
 	
-		randomize()
-		token = str(randi()).sha256_text() + str(int(Time.get_unix_time_from_system()))
-		var gameserver = "GameServer1" # this will have to be replaced with a load balancer
-		GameServers.DistributeLoginToken(token, gameserver)
 	print("Authentication result sent to gateway server: ", gateway_id)
 	rpc_id(gateway_id, "AuthenticationResults", result, player_id, token)
 
@@ -51,3 +56,46 @@ func AuthenticatePlayer(username, password, player_id):
 func AuthenticationResults(result, player_id, token):
 	print(str(result) + str(player_id) + token)
 	pass
+
+
+@rpc(any_peer)
+func CreateAccount(username, password, player_id):
+	var gateway_id = multiplayer.get_remote_sender_id()
+	var result
+	var message
+	if PlayerData.player_ids.has(username):
+		result = false
+		message = 2
+	else:
+		result = true
+		message = 3
+		var salt = GenerateSalt()
+		var hashed_password = GenerateHashedPassword(password, salt)
+		PlayerData.player_ids[username] = {"password": hashed_password, "salt": salt}
+		PlayerData.SavePlayerIDs()
+	
+	rpc_id(gateway_id, "CreateAccountResults", result, player_id, message)
+
+
+@rpc
+func CreateAccountResults(_result, _player_id, _message):
+	# used for rpc checksum
+	pass
+
+
+func GenerateSalt():
+	randomize()
+	var salt = str(randi()).sha256_text()
+	print("Salt: " + salt)
+	return salt
+
+
+func GenerateHashedPassword(password, salt):
+	var hashed_password = password
+	var rounds = pow(2, 18)
+	print("Hashed password as input: " + hashed_password)
+	while rounds > 0:
+		hashed_password = (hashed_password + salt).sha256_text()
+		rounds -= 1
+	print("Final Hashed Password: " + hashed_password)
+	return hashed_password
--- Scenes/Singletons/PlayerData.gd
+++ Scenes/Singletons/PlayerData.gd
@@ -4,6 +4,11 @@ var player_ids
 
 
 func _ready():
-	var player_ids_file = FileAccess.open("res://Data/player-data.json", FileAccess.READ)
+	var player_ids_file = FileAccess.open("user://PlayerIDs.json", FileAccess.READ)
 	var player_data_json = JSON.parse_string(player_ids_file.get_as_text())
 	player_ids = player_data_json
+
+
+func SavePlayerIDs():
+	var file = FileAccess.open("user://PlayerIDs.json", FileAccess.READ_WRITE)
+	file.store_line(JSON.stringify(player_ids, "\t"))
